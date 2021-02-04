c2349851a2fda95bbac88b1f464c3b3ba320c27f
AudeP <aude.plana@gmail.com>
2021-02-04 15:35:11 +0100

    css form new recipe done

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -8,6 +8,7 @@ use phpDocumentor\Reflection\Types\Collection;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
